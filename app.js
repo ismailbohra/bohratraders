45e534ef6c70d782916927f99d36aa3fb3bec916
ismail bohra <ismailbohra99@gmail.com>
2023-06-03 01:32:49 +0530

    changes

--- app.js
+++ app.js
@@ -44,6 +44,12 @@ app.use('/v1', routes);
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 module.exports = app;
